5f89f8c76ada22b603fd7dbd1388b025ee40d5e2
grakzanchetta <grakzanchetaa@gmail.com>
2023-01-09 20:17:51 -0300

    solved

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Tamanho da base da pirâmide?");
+        int valor = myObj.nextInt();
+
+        for (int i = 1; i <= valor; i++) {
+            String linha = "";
+            for (int j = 1; j <= i; j++) {
+                linha += i;
+            }
+            System.out.println(linha);
+        }
+
     }
 }
